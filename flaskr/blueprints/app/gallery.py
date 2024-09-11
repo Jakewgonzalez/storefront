240e69f351f9cbc1df786cbdd9e38c3f3561c0a0
jakewgonzalez <jake.w.gonzalez@gmail.com>
2024-09-11 16:00:17 -0400

    added image route

--- flaskr/blueprints/app/gallery.py
+++ flaskr/blueprints/app/gallery.py
@@ -5,3 +5,7 @@ gallery_bp = Blueprint('gallery', __name__, template_folder='templates')
 @gallery_bp.route('/')
 def index():
     return render_template('gallery.html')
+
+@gallery_bp.route('/image/<image_name>')
+def image(image_name):
+    return render_template('image.html', image_name=image_name)
